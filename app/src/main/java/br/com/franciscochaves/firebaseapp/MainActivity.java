aa30d12d4e0ee6badd89c9ec98c67010ce8bdeae
Francisco Chaves <francisco.jschaves@gmail.com>
2019-06-09 00:31:43 -0300

    Criando novos nó filho e adicionnado valor no database realtime

--- app/src/main/java/br/com/franciscochaves/firebaseapp/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/firebaseapp/MainActivity.java
@@ -11,11 +11,16 @@ public class MainActivity extends AppCompatActivity {
     // Obtendo o nó raiz do database
     private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
+    // Criando nó filho
+    private DatabaseReference usuarioReference = databaseReference.child("usuario");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseReference.child("pontos").setValue("100");
+        //databaseReference.child("pontos").setValue("100");
+        usuarioReference.child("001").child("nome").setValue("Francisco Chaves");
+        usuarioReference.child("002").child("nome").setValue("Maria Almeida");
     }
 }
