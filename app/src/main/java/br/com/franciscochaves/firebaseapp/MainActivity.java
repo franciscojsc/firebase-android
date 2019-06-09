34e4a8f910b02fd122081b05c66a47a072f89f2a
Francisco Chaves <francisco.jschaves@gmail.com>
2019-06-09 00:21:28 -0300

    Criando nó filho e adicionnado valor no database realtime

--- app/src/main/java/br/com/franciscochaves/firebaseapp/MainActivity.java
+++ app/src/main/java/br/com/franciscochaves/firebaseapp/MainActivity.java
@@ -3,11 +3,19 @@ package br.com.franciscochaves.firebaseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Obtendo o nó raiz do database
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseReference.child("pontos").setValue("100");
     }
 }
